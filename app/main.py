8d21e0660ba6181fef947c86b30dd04600b297a8
James Yin <jamesyinbaare@gmail.com>
2023-10-03 08:06:39 +0100

    Add Dockerfile and docker-compose.yml

--- app/main.py
+++ app/main.py
@@ -1,27 +1,10 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-
 from fastapi import FastAPI
-
-
-@dataclass
-class Item:
-    name: str
-    price: float
-    tags: list[str] = field(default_factory=list)
-    description: str | None = None
-    tax: float | None = None
-
 
 app = FastAPI()
 
 
-@app.get("/items/next", response_model=Item)
-async def read_next_item():
-    return {
-        "name": "Island In The Moon",
-        "price": 14.6,
-        "description": "A place to be playin and having fun",
-        "tags": ["breater"],
-    }
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
